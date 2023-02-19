ede9a2a59117eb36277ea9c73240cf78da65b7c0
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-02-19 10:05:47 +0100

    First Basic Programs in Java

---
+++ src/main/java/org/example/BasicPrograms/AddTwoNumbers.java
@@ -0,0 +1,15 @@
+package org.example.BasicPrograms;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int number1 = 5;
+        int number2 = 10;
+
+        int sum = number1 + number2;
+
+        System.out.println("Sum of two numbers equals: " + sum);
+        System.out.println(number1 + " + " + number2 + " equals " + sum);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,29 +3,11 @@ package org.example;
 
 public class Main {
 
-    int x = 5;
-
     public static void main(String[] args) {
-
-        Main myObj = new Main();
-
-        System.out.println(myObj.x);
         System.out.println("Hello world!");
-        String name = "John";
-        System.out.println(name);
-
-        int number1 = 20;
-        int number2 = 10;
-
-        System.out.println(number1 + number2);
-
-        String[] names = {"James", "Alex"};
-
-        for (int i =0; i <= names.length; i++) {
-            System.out.println(names[i]);
-        }
     }
 }
+
 
 
 
