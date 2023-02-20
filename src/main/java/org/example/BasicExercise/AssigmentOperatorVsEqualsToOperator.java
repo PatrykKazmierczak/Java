cfa6fe746466217a0ac007c7a48235895c4de609
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-02-20 21:07:26 +0100

    update

---
+++ src/main/java/org/example/BasicExercise/AssigmentOperatorVsEqualsToOperator.java
@@ -0,0 +1,11 @@
+package org.example.BasicExercise;
+
+public class AssigmentOperatorVsEqualsToOperator {
+    public static void main(String[] args) {
+
+        boolean isCar = false;
+        if(isCar == true) {
+            System.out.println("This is not supposed to happen");
+        }
+    }
+}
