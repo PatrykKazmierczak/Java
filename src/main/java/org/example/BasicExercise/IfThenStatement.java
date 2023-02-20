6742406ab6f58724ee7517a107eb0832a51f6124
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-02-20 20:26:21 +0100

    update

---
+++ src/main/java/org/example/BasicExercise/IfThenStatement.java
@@ -0,0 +1,17 @@
+package org.example.BasicExercise;
+
+public class IfThenStatement {
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if(isAlien == false)
+            System.out.println("It is not Alien");
+
+
+        boolean isSmart = false;
+        if (isSmart == false)
+            System.out.println("He is not smart");
+        else
+            System.out.println("He is smart");
+    }
+}
