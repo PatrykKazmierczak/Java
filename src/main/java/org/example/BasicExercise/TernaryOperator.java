1ca44613aab31f519aa3421549fa066e60800e18
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-02-21 09:16:50 +0100

    update

--- src/main/java/org/example/BasicExercise/TernaryOperator.java
+++ src/main/java/org/example/BasicExercise/TernaryOperator.java
@@ -1,5 +1,7 @@
 package org.example.BasicExercise;
 
+import java.util.Scanner;
+
 public class TernaryOperator {
 
     public static void main(String[] args) {
@@ -10,5 +12,25 @@ public class TernaryOperator {
         if(isDomestic) {
             System.out.println("This car is domestic to our country");
         }
+
+        int numb1 = 10;
+        int numb2 = 8;
+        int max;
+
+        System.out.println("First number: " + numb1);
+        System.out.println("Second number: " + numb2);
+
+        max = numb1>numb2  ? numb1 : numb2;
+
+        System.out.println("Maximum is = " + max);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        double marks = sc.nextDouble();
+
+        String result = (marks > 40) ? "pass" : "fail";
+
+        System.out.println("Your " + result + " the exam");
+        sc.close();
     }
 }
