02431ee980844f9a0328b020b4d4c9e1584979f5
Patryk-Kazmierczak <patrykkazmierczak0@gmail.com>
2023-02-22 08:18:23 +0100

    update

--- src/main/java/org/example/BasicExercise/CodeBlocksAndIfThenElseStatements.java
+++ src/main/java/org/example/BasicExercise/CodeBlocksAndIfThenElseStatements.java
@@ -4,16 +4,17 @@ public class CodeBlocksAndIfThenElseStatements {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 5000;
+        int score = 900;
         int levelCompleted = 5;
         int bonus = 100;
 
-        if(score <= 5000 && score > 1000) {
+        if (score <= 5000 && score > 1000) {
             System.out.println("Your score was less than " + score + " but grater than 1000");
         } else if (score < 1000) {
             System.out.println("Your score was less than 1000");
-        }
+        } else {
             System.out.println("Got here");
         }
     }
+}
 
