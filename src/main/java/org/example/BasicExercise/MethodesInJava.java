ad72bd7db42afe4af50a2238be0330497b370acc
PatrykKazmierczak <patrykkazmierczak0@gmail.com>
2023-02-24 21:44:44 +0100

    update

--- src/main/java/org/example/BasicExercise/MethodesInJava.java
+++ src/main/java/org/example/BasicExercise/MethodesInJava.java
@@ -2,9 +2,41 @@ package org.example.BasicExercise;
 
 public class MethodesInJava {
     public static void main(String[] args) {
-        calculateScore(true, 900, 5, 100);
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+
+        int highScore = calculateScore(gameOver,score, levelCompleted, bonus);
+        System.out.println("High highScore equals: " + highScore);
+
+        score = 950;
+        levelCompleted = 5;
+        bonus = 100;
+
+        System.out.println("Next highScore is: " +
+                calculateScore(gameOver, score, levelCompleted, bonus));
+
     }
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+//    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+//
+////        boolean gameOver = true;
+////        int score = 800;
+////        int levelCompleted = 5;
+////        int bonus = 100;
+//
+//        int finalScore = score;
+//
+//        if (gameOver == true) {
+//            finalScore += (levelCompleted * bonus);
+//            System.out.println("Your final score was " + finalScore);
+//        }
+//    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
 //        boolean gameOver = true;
 //        int score = 800;
@@ -13,9 +45,15 @@ public class MethodesInJava {
 
         int finalScore = score;
 
-        if (gameOver == true) {
+        if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 1000;
         }
+        return finalScore;
     }
 }
+
+
+
+
+
